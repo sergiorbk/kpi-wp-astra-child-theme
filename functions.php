3ab25dbf70c1ae887d4ab7205d98387cddc506da
sergiorbk <sergey24rybak@gmail.com>
2024-10-23 20:45:35 +0300

    update_astra_color_palettes() function was implemented in order to set an only color palette according to the brand book.

--- functions.php
+++ functions.php
@@ -21,17 +21,11 @@ function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 
 }
-
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
+// Do not remove the code above!!!
 
 
-// THEME SETTINGS
-// Hides the "Customise CSS" option in theme settings
-function remove_custom_css_section( $wp_customize ) {
-    $wp_customize->remove_section( 'custom_css' );
-}
-add_action( 'customize_register', 'remove_custom_css_section', 15 );
-
+// Write your code below
 
 // CUSTOM FOOTER BLOCK
 function kpi_custom_footer() {
@@ -57,3 +51,42 @@ function kpi_custom_footer() {
     </footer>
 <?php
 }
+
+
+
+// ASTRA THEME settings BEGIN ==================================================
+
+// Hides the "Customise CSS" option in theme settings
+function remove_custom_css_section( $wp_customize ) {
+    $wp_customize->remove_section( 'custom_css' );
+}
+add_action( 'customize_register', 'remove_custom_css_section', 15 );
+
+
+// Sets a single color palette according to the brand book
+function update_astra_color_palettes() {
+    $new_palette = array(
+        'currentPalette' => 'palette_1',
+        'palettes' => array(
+            'palette_1' => array(
+                '#1c396e',
+                '#008acf',
+                '#1062a3',
+                '#0d5690',
+                '#004f7f',
+                '#ffffff',
+                '#f07d00',
+                '#ec6605',
+                '#7f0d38',
+            ),
+        ),
+        'presets' => array(),
+        'flag' => true,
+    );
+    update_option('astra-color-palettes', $new_palette);
+}
+add_action('init', 'update_astra_color_palettes');
+
+
+
+// ASTRA THEME settings END ==================================================
